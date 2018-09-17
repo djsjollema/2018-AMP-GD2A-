54f614e146f63445c338b372feaaee63dfabb2cf
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-17 14:55:02 +0200

    random points

--- html/myPoint/script.js
+++ html/myPoint/script.js
@@ -2,13 +2,29 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let myPoints = [];
 
-
-
-for (var i = 0; i < 1000; i++) {
-  let myPoint = new Point(getRandomNumber(canvas.width),getRandomNumber(canvas.height));
-  myPoint.draw();
+function setUp(){
+  for (var i = 0; i < 10; i++) {
+    let myPoint = new Point(getRandomNumber(canvas.width),getRandomNumber(canvas.height));
+    myPoints.push(myPoint);
+  }
+  update();
 }
+
+
+function update(){
+  requestAnimationFrame(update);
+  let myPoint = new Point(getRandomNumber(canvas.width),getRandomNumber(canvas.height));
+  myPoints.push(myPoint);
+  context.clearRect(0,0,canvas.width,canvas.height);
+  for (let i = 0; i < myPoints.length; i++) {
+    myPoints[i].draw(context);
+  }
+
+}
+
+setUp();
 
 function getRandomNumber(max){
   return Math.random()*max;
