0c693b7a1f9327cdb221c2f3d12af073b25439c0
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-24 12:37:59 +0200

    pos vel acc stage4

--- html/03PositionVelocityAndAcceleration/script.js
+++ html/03PositionVelocityAndAcceleration/script.js
@@ -8,7 +8,7 @@ let kineticObject = {};
 function setUp(){
   kineticObject.point = new Point(200,300,"red");
   kineticObject.pos = new Vector2d(800,300);
-  kineticObject.vel = new Vector2d(1,2);
+  kineticObject.vel = new Vector2d(8,5);
   kineticObject.acc = new Vector2d(0,0);
 
   //kineticObject.point.position(kineticObject.pos);
@@ -19,6 +19,12 @@ function setUp(){
 function update(){
   context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(update);
+  if(kineticObject.pos.dx<0 || kineticObject.pos.dx > canvas.width){
+    kineticObject.vel.dx = -kineticObject.vel.dx;
+  }
+  if(kineticObject.pos.dy<0 || kineticObject.pos.dy > canvas.height){
+    kineticObject.vel.dy = -kineticObject.vel.dy;
+  }
   kineticObject.pos.add(kineticObject.vel);
   kineticObject.point.position(kineticObject.pos);
   kineticObject.point.draw(context);
