7d829ef410fcf9800c3c52cb9c74940e520c79c1
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-11-26 13:37:53 +0100

    15_2

--- html/16mousePointer/script.js
+++ html/16mousePointer/script.js
@@ -5,9 +5,52 @@ canvas.height = window.innerHeight;
 
 let midX = canvas.width/2;
 let midY = canvas.height/2;
+let mouse = {};
+let rot = 0;
 
 function animate(){
-  context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
+  //context.clearRect(0,0,1000,1000);
+  context.fillStyle = "rgba(255,255,255,0.2)";
+  context.fillRect(0,0,canvas.width,canvas.height);
+  context.save();
+  context.translate(midX,midY);
+  if(mouse.x > midX){
+    rot = Math.atan((mouse.y - midY)/(mouse.x - midX));
 
+  } else {
+    rot = Math.atan((mouse.y - midY)/(mouse.x - midX)) - Math.PI;
+
+  }
+  context.rotate(rot);
+  drawArrow(context);
+  context.restore();
 }
+
+animate();
+
+function drawArrow(context){
+  let shaftHeight = 7;
+  let shaftWidth = 80;
+  let arrowHeight = 15;
+  let arrowWidth = 30;
+  context.fillStyle = "rgba(255,0,0,0.4)";
+  context.beginPath();
+  context.moveTo(0,0);
+  context.lineTo(0,-shaftHeight);
+  context.lineTo(shaftWidth,-shaftHeight);
+  context.lineTo(shaftWidth,-arrowHeight);
+  context.lineTo(shaftWidth+arrowWidth,0);
+  context.lineTo(shaftWidth,arrowHeight);
+  context.lineTo(shaftWidth,shaftHeight);
+  context.lineTo(0,shaftHeight);
+  context.closePath();
+  context.stroke();
+
+  context.fill();
+}
+
+addEventListener('mousemove',(evt)=>{
+  mouse.x = evt.clientX;
+  mouse.y = evt.clientY;
+})
