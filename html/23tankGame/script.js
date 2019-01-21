ff83b8f61a1e964356d24f252f8ccc1124337538
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-01-21 13:33:56 +0100

    24_1

--- html/23tankGame/script.js
+++ html/23tankGame/script.js
@@ -3,9 +3,11 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
+let startTime,currentTime,dt,fps;
 
 function setUp(){
+  startTime = new Date(); // set starttime
+  fps = 1; //frames per second
   update();
 }
 
@@ -13,6 +15,12 @@ function setUp(){
 function update(){
   requestAnimationFrame(update);
   context.clearRect(0,0,canvas.width,canvas.height);
+  currentTime = new Date();
+  dt = (currentTime - startTime)/1000;
+  if(dt >  1/fps){
+    console.log(dt);
+    startTime = new Date();
+  }
 }
 
 setUp();
