edf4ea19c516dc7b2b2ebf00374042b9f35e6299
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-27 14:32:07 +0100

    25_2

--- html/25reflectingLine/script.js
+++ html/25reflectingLine/script.js
@@ -2,3 +2,23 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let A,B,l;
+
+function setUp(){
+  A = new Point(100,200,20,"yellow");
+  B = new Point(600,400,20,"yellow");
+  l = new LinearFunction(1,1);
+
+  animate();
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,canvas.width,canvas.height);
+  A.draw(context);
+  B.draw(context);
+  l.draw(context);
+}
+
+setUp();
